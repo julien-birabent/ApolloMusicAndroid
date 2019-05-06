6ee74e38690c78e6788d5ec9a65996396c484d80
Julien <jul.birabent@gmail.com>
2019-05-05 22:06:49 -0400

    simplifying activityExtensions methods

--- app/src/main/java/julienbirabent/apollomusic/extensions/ActivityExtensions.kt
+++ app/src/main/java/julienbirabent/apollomusic/extensions/ActivityExtensions.kt
@@ -1,38 +1,31 @@
 package julienbirabent.apollomusic.extensions
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 
-inline fun <reified T : Any> Activity.launchActivity (
+inline fun <reified T : Any> Activity.launchActivity(
     requestCode: Int = -1,
     options: Bundle? = null,
-    noinline init: Intent.() -> Unit = {})
-{
+    noinline init: Intent.() -> Unit = {}
+) {
     val intent = newIntent<T>(this)
     intent.init()
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-    {
-        startActivityForResult(intent, requestCode, options)
-    } else {
-        startActivityForResult(intent, requestCode)
-    }
+    startActivityForResult(intent, requestCode, options)
+
 }
 
-inline fun <reified T : Any> Context.launchActivity (
+@SuppressLint("ObsoleteSdkInt")
+inline fun <reified T : Any> Context.launchActivity(
     options: Bundle? = null,
-    noinline init: Intent.() -> Unit = {})
-{
+    noinline init: Intent.() -> Unit = {}
+) {
     val intent = newIntent<T>(this)
     intent.init()
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-    {
-        startActivity(intent, options)
-    } else {
-        startActivity(intent)
-    }
+    startActivity(intent, options)
+
 }
 
 inline fun <reified T : Any> newIntent(context: Context): Intent =
