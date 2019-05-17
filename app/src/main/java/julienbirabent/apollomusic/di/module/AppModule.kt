8b7f1a27fb8420f71b822f37d0b9004e276e7aa9
Julien <jul.birabent@gmail.com>
2019-05-17 15:12:11 -0400

    Room dependency + inject sharedPreferences

--- app/src/main/java/julienbirabent/apollomusic/di/module/AppModule.kt
+++ app/src/main/java/julienbirabent/apollomusic/di/module/AppModule.kt
@@ -5,11 +5,28 @@ import android.content.Context
 import dagger.Binds
 import dagger.Module
 import javax.inject.Named
+import javax.inject.Singleton
+import dagger.Provides
+import android.content.SharedPreferences
+
+
+
+
 
 @Module
-abstract class AppModule{
+class AppModule{
 
-    @Binds
+    private val SHARED_PACKAGE = "base_shared_preferences"
+
+    @Provides
     @Named("ApplicationContext")
-    abstract fun provideContext(application: Application): Context
+    fun provideContext(application: Application): Context {
+        return application
+    }
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences(SHARED_PACKAGE, Context.MODE_PRIVATE)
+    }
 }
